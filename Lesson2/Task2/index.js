17437da7c94be488670059039d0987d64c990970
Nazar <sikirinskijn@gmailcom>
2020-07-28 18:34:18 +0300

    type vibes

---
+++ Lesson2/Task2/index.js
@@ -0,0 +1,28 @@
+const a = 17;
+console.log('a:', typeof a);
+const b = 17.17;
+console.log('b:', typeof b);
+const c = '17';
+console.log('c:', typeof c);
+const d = "17";
+console.log('d:', typeof d);
+const e = "I'm a string";
+console.log('e:', typeof e);
+const f = false;
+console.log('f:', typeof f);
+const g = true;
+console.log('g:', typeof g);
+const h = null;
+console.log('h:', typeof h);
+const i = undefined;
+console.log('i:', typeof i);
+const j = Symbol("I'm a Symbol");
+console.log('j:', typeof j);
+const k = { name: 'John' };
+console.log('k:', typeof k);
+const l = [1, 2, 3, 4];
+console.log('l:', typeof l);
+const m = function run() {
+  return 17;
+};
+console.log('m:', typeof m);
