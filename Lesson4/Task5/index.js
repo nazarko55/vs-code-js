f4cc8cb95b63264b36f0265f70342f4cfb619683
Nazar <sikirinskijn@gmailcom>
2020-07-30 18:54:02 +0300

    xsx

--- Lesson4/Task5/index.js
+++ Lesson4/Task5/index.js
@@ -5,12 +5,12 @@ let result = 0;
 for (let i = m; i <= n; i++) {
   if (i % 2 === 0 && i % 4 !== 0) {
     result += i;
-    console.log(5);
   } else if (i % 3 === 0) {
     result -= i;
   } else if (i % 4 === 0) {
     result *= i;
   }
+  console.log(5);
   if (i % 5 === 0) {
     console.log(i);
   }
