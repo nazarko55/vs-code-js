23eac779edc13bf594019f27fdf1656146ee2ce5
Nazar <sikirinskijn@gmailcom>
2020-07-30 22:55:42 +0300

    qqw

--- Lesson4/Task5/index.js
+++ Lesson4/Task5/index.js
@@ -1,17 +1,20 @@
-const m = 1;
-const n = 18;
+const m = 4;
+const n = 14;
 let result = 0;
-
 for (let i = m; i <= n; i++) {
-  if (i % 2 === 0 && i % 4 !== 0) {
+  if (i % 2 === 0 && !(i % 4 === 0) && !(i % 5 === 0)) {
     result += i;
-  } else if (i % 3 === 0) {
+    continue;
+  } else if (i % 3 === 0 && !(i % 5 === 0)) {
     result -= i;
-  } else if (i % 4 === 0) {
+    continue;
+  } else if (i % 4 === 0 && !(i % 5 === 0)) {
     result *= i;
-  }
-  console.log(5);
-  if (i % 5 === 0) {
+    continue;
+  } else if (i % 5 === 0) {
     console.log(i);
+  } else {
+    continue
   }
 }
+
