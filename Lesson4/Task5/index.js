20f30dfd2c99d4c7d22d73d237d45b3c6eafaeca
Nazar <sikirinskijn@gmailcom>
2020-07-30 18:52:06 +0300

    qw

--- Lesson4/Task5/index.js
+++ Lesson4/Task5/index.js
@@ -5,6 +5,7 @@ let result = 0;
 for (let i = m; i <= n; i++) {
   if (i % 2 === 0 && i % 4 !== 0) {
     result += i;
+    console.log(5);
   } else if (i % 3 === 0) {
     result -= i;
   } else if (i % 4 === 0) {
