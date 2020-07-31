7185ec711f34c4d0e30e3ea14b1928b16f027c72
Nazar <sikirinskijn@gmailcom>
2020-07-31 17:04:11 +0300

    uyt

--- Lesson5/Task9/index.js
+++ Lesson5/Task9/index.js
@@ -1,14 +1,15 @@
-function getPrimes(n) {
-
-  next:
-  for (let i = 2; i <= n; i++) {
-    for (let k = 2; k <= i; k++) {
-      if (i % k === 0) {
-        continue next;
-      }
+function isPrimes(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
     }
-    console.log(i);
+  }
+  return true;
+}
+function getPrimes(n) {
+  for (let i = 2; i <= n; i++) {
+    if (isPrimes(i)) {
+      console.log(i);
+    }
   }
 }
-
-getPrimes(20);
