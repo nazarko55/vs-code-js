4902c10eb2dfd98377718a5fb91d8de39ef6a38f
Nazar <sikirinskijn@gmailcom>
2020-08-01 00:53:43 +0300

    fff

---
+++ Lesson6/Task1/index.js
@@ -0,0 +1,8 @@
+function getArrayBound(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  return [arr.length, arr[0], arr[arr.length - 1]];
+}
+const arrayBound = getArrayBound([]);
+console.log(arrayBound); 
