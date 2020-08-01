2aa71f23ff85dd3700ba0a4b2c8a769b6b716bc1
Nazar <sikirinskijn@gmailcom>
2020-08-01 22:31:50 +0300

    ggg

--- Lesson7/Task2/index.js
+++ Lesson7/Task2/index.js
@@ -1,7 +1,5 @@
-const sortDesc = numbers => {
-  numbers.sort((a, b) => b - a);
-
-  return numbers;
-};
+const sortDesc = numbers =>
+  numbers.slice().sort((a, b) => b - a);
 const arr = [12, 45, 89, 90, 43, 67, 81];
 console.log(sortDesc(arr));
+console.log(arr);
