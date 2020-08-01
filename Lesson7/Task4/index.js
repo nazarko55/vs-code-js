020b0b3bc250aa84a7a7c4d4b38e4bd2506a9fbc
Nazar <sikirinskijn@gmailcom>
2020-08-02 01:52:11 +0300

    jhyujb

--- Lesson7/Task4/index.js
+++ Lesson7/Task4/index.js
@@ -1,8 +1,8 @@
-const getMessagesForPassedStudents = (allStudents, failedStudents) =>
+const getMessagesForBestStudents = (allStudents, failedStudents) =>
   allStudents
     .filter(name => !failedStudents.includes(name))
     .map(name => 'Good job, ' + name);
 
 const allStudents = ['Ann', 'Tom', 'Bob', 'Kate'];
 const failedStudents = ['Tom', 'Bob'];
-console.log(getMessagesForPassedStudents(allStudents, failedStudents));
+console.log(getMessagesForBestStudents(allStudents, failedStudents));
