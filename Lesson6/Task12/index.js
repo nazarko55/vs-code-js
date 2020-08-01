4bcb005d34dc3038776960c9460d6fa94c4f34b6
Nazar <sikirinskijn@gmailcom>
2020-08-01 16:25:45 +0300

    gggot

--- Lesson6/Task12/index.js
+++ Lesson6/Task12/index.js
@@ -1,8 +1,8 @@
 let number = [22, 44, 66, 89, 12]
-function cloneArr(arr) {
+function checker(arr) {
   if (!Array.isArray(arr)) return null;
   let sum = Math.max.apply(null, arr) + Math.min.apply(null, arr);
   return (sum > 1000)
 
 }
-console.log(cloneArr([]));
+console.log(checker([]));
