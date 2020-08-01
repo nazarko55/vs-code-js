8f3aa720e6206a74333b484bff82859085d5cb42
Nazar <sikirinskijn@gmailcom>
2020-08-01 22:20:11 +0300

    good

---
+++ Lesson7/Task2/index.js
@@ -0,0 +1,13 @@
+const sortArray = numbers => {
+  function somepare(a, b) {
+    if (b < a) {
+      return 1;
+    }
+    return -1;
+  }
+  numbers.sort(somepare);
+
+  return numbers;
+};
+const arr = [12, 45, 89, 90, 43, 67, 81];
+console.log(sortArray(arr));
