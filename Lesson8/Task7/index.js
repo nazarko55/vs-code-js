2e03a0917d1361c199ff5bcb69d563b8bd9081c1
Nazar <sikirinskijn@gmailcom>
2020-08-04 19:12:09 +0300

    ttt

---
+++ Lesson8/Task7/index.js
@@ -0,0 +1,29 @@
+let obj1 = {
+  name: 'Anton',
+  surname: 'Vorotnikov1',
+  age: 26,
+}
+
+let obj2 = {
+  name: 'Mikhail',
+  surname: 'Vorotnikov2',
+  age: 23,
+}
+
+function mergeObjectsV1(obj1, obj2) {
+  let res = Object.assign({}, obj1, obj2);
+  return res;
+}
+console.log(mergeObjectsV1(obj1, obj2));
+
+function mergeObjectsV2(obj1, obj2) {
+  let rest = Object.assign({}, obj2, obj1);
+  return rest;
+}
+console.log(mergeObjectsV2(obj1, obj2));
+
+function mergeObjectsV3(obj1, obj2) {
+  let resty = Object.assign(...obj1, ...obj2);
+  return resty;
+}
+console.log(mergeObjectsV3(obj1, obj2));
