de7b7b21a83acfcba71c82a6efe1e3e7acb10533
Nazar <sikirinskijn@gmailcom>
2020-08-04 13:42:43 +0300

    777

---
+++ Lesson8/Task4/index.js
@@ -0,0 +1,12 @@
+const user = {
+  name: 'Tom',
+  age: 17,
+};
+user.hobby = 'football';
+const anotherKey = 'married';
+user.anotherKey = false;
+user['favorite music'] = 'rock';
+user.address = {};
+user.address.country = 'Ukraine';
+user.address.building = 17;
+console.log(user);
