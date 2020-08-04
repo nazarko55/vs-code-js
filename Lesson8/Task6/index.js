926fb0c4b8fe809845e62d6d021fe8366c1d54a4
Nazar <sikirinskijn@gmailcom>
2020-08-04 15:54:43 +0300

    ee

--- Lesson8/Task6/index.js
+++ Lesson8/Task6/index.js
@@ -1,15 +1,18 @@
 const addPropertyV1 = (userData, userId) => {
-  userData.element = userId;
+  userData.id = userId;
   return userData;
 };
+
 const addPropertyV2 = (userData, userId) => {
   const result = Object.assign(userData, { id: userId });
   return result;
 };
+
 const addPropertyV3 = (userData, userId) => {
-  const result1 = Object.assign({}, userData, { id: userId });
-  return result1;
+  const result = Object.assign({}, userData, { id: userId });
+  return result;
 };
+
 const addPropertyV4 = (userData, userId) => {
   const result = Object.assign({ ...userData }, { id: userId });
   return result;
@@ -17,12 +20,9 @@ const addPropertyV4 = (userData, userId) => {
 
 const user = {
   name: 'King',
-
 };
 
-console.log(addPropertyV1(user, '0987654321'))
-console.log(addPropertyV2(user, ''))
-console.log(addPropertyV3(user, '0987654321'))
-console.log(addPropertyV4(user, '0987654321'))
-
-
+// console.log(addPropertyV1(user, '0987654321'))
+// console.log(addPropertyV2(user, '0987654321'))
+// console.log(addPropertyV3(user, '0987654321'))
+// console.log(addPropertyV4(user, '0987654321'))
