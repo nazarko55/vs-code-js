95c42312c8bf42651c4003a84c0c3682dbcb65bb
Nazar <sikirinskijn@gmailcom>
2020-08-04 12:23:59 +0300

    fff

--- Lesson8/Task2/index.js
+++ Lesson8/Task2/index.js
@@ -1,14 +1,18 @@
-const user = {
-  name: 'Tom',
-  key: 17,
-};
 
 const concatProps = obj => {
   let arr = [];
 
   for (let key in obj) {
-    arr = arr.concat(obj[key]);
+    arr.push(obj[key])
+    //arr = arr.concat(obj[key]);
   }
   return arr;
 }
+
+const user = {
+  name: 'John Doe',
+  key: 17,
+  interest: 'football'
+};
+
 console.log(concatProps(user));
