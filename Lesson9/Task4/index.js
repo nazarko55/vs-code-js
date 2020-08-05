64e3854a8ad4285161c363e229ac170a91a3e115
Nazar <sikirinskijn@gmailcom>
2020-08-05 17:35:56 +0300

    ytr

---
+++ Lesson9/Task4/index.js
@@ -0,0 +1,10 @@
+const dayTransaction = [
+  { userId: 22, amount: 55, operation: 'sell' },
+  { userId: 38, amount: 210, operation: 'sell' },
+  { userId: 274, amount: 112, operation: 'sell' },
+]
+
+function getTotalRevenue(arr) {
+  return dayTransaction.map((el) => el.amount).reduce((a, b) => a + b, 0)
+}
+console.log(getTotalRevenue());
