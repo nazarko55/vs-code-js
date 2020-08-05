24d44a8ece834ccbe3b6e421a85b01d79ff0fd01
Nazar <sikirinskijn@gmailcom>
2020-08-05 11:35:22 +0300

    cc

--- Lesson8/Task12/index.js
+++ Lesson8/Task12/index.js
@@ -1,24 +1,15 @@
-// algo
-// gather all arguments
-// multiply
-
-function multiply(...args) {
-  console.log(args);
-  let res = 1;
-  args.forEach(el => {
-    res = res * el;
-  });
-  return res;
+// algo 
+// create empty array
+// iterate array & call callback
+// input: [10, 20, 30, 1, 8]
+// output: [30]
+function filterArray(arr, callback) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) result.push(arr[i])
+  }
+  return result
 }
-
-
-/////////////////// 2
-
-function multiply(...args) {
-  return args.reduce((acc, current) => res * current);
-}
-const multipleRes = multiply(10, 12, 3, 71, 4, 8);
-console.log(multipleRes);
-
-
-
+let array = [10, 30, 1, 8, 122];
+const callback = element => element > 20;
+console.log(filterArray(array, callback))
