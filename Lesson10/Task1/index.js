db21b2ae53fef0033aeaa3dde23d9b8aca6ade03
Nazar <sikirinskijn@gmailcom>
2020-08-05 23:58:29 +0300

    ee

--- Lesson10/Task1/index.js
+++ Lesson10/Task1/index.js
@@ -2,7 +2,7 @@ const getFiniteNumbers = arr => arr.filter(num => Number.isFinite(num));
 const getFiniteNumbersV2 = arr => arr.filter(num => isFinite(num));
 const getNaN = arr => arr.filter(num => Number.isNaN(num));
 const getNaNV2 = arr => arr.filter(num => isNaN(num));
-const getInetegers = arr => arr.filter(num => Number.isInteger(num));
+const getIntegers = arr => arr.filter(num => Number.isInteger(num));
 const arr = [17.23, '13.45', 'hello', 66, NaN, 'NaN', null];
-let res = getInetegers(arr)
+let res = getIntegers(arr)
 console.log(res);
