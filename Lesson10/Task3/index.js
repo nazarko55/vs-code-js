934c63b6bcb609591cd2c29e24cc67d752d83358
Nazar <sikirinskijn@gmailcom>
2020-08-06 00:42:19 +0300

    yuui

---
+++ Lesson10/Task3/index.js
@@ -0,0 +1,8 @@
+const multiRound = (num, accuracy) => [
+  Math.floor((num) * (10 ** accuracy)) / 10 ** accuracy,
+  Math.round((num) * (10 ** accuracy)) / 10 ** accuracy,
+  Math.ceil((num) * (10 ** accuracy)) / 10 ** accuracy,
+  Math.trunc((num) * (10 ** accuracy)) / 10 ** accuracy, +num.toFixed(accuracy)
+
+]
+console.log(multiRound(5.6789, 2));
