d8cd57400373ebc69954ae3bddc3d465f75ecbba
Nazar <sikirinskijn@gmailcom>
2020-08-06 13:48:25 +0300

    bhgt

--- Lesson10/Task4/index.js
+++ Lesson10/Task4/index.js
@@ -1,4 +1,5 @@
 const getMaxAbsoluteNumber = arr => {
+  if (!Array.isArray(arr)) return null;
   const absoluteValues = arr
     .map(num => Math.abs(num));
   const max = Math.max(...absoluteValues);
