1a532427d9eb298b7cfc4fe1d6c01dc1249dea31
Nazar <sikirinskijn@gmailcom>
2020-08-06 18:13:23 +0300

    ytuii

--- Lesson11/Task2/index.js
+++ Lesson11/Task2/index.js
@@ -24,7 +24,7 @@ const sortContacts = contacts => {
   if (!Array.isArray(contacts)) return null;
   const result = contacts
     .sort((a, b) => {
-      return a.name.localeCompare(b.name);
+      return b.name.localeCompare(a.name);
     });
   return result;
 };
