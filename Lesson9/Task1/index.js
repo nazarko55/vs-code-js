57ea4061c37effebba3c10fccf0c5da6f5aecdc5
Nazar <sikirinskijn@gmailcom>
2020-08-06 11:49:29 +0300

    bnh

--- Lesson9/Task1/index.js
+++ Lesson9/Task1/index.js
@@ -1,10 +1,33 @@
-const getAdults = usersObj => Object.entries(usersObj)
-  .filter(user => user[1] >= 18)
-  .map(user => user[0]);
+//
 
-const users = {
-  'Tom': 17,
-  'John': 22,
-  'Baba': 19,
-};
-console.log(getAdults(users));
+function addPropertyV1(userData, userId) {
+  userData.id = userId;
+  return userData;
+}
+function addPropertyV2(userData, userId) {
+  //input: Object
+  //output: object
+  Object.assign(userData, { id: userId });
+  return userData;
+}
+function addPropertyV3(userData, userId) {
+  return Object.assign({}, userData, { id: userId });
+
+}
+function addPropertyV4(userData, userId) {
+  userData.id = userId;
+  return userData;
+}
+
+const obj = {
+
+  name: 'Andrey',
+  age: 100,
+  city: 'test',
+}
+
+console.log(addPropertyV1(obj, '2222'));
+console.log(addPropertyV2(obj, '333'));
+console.log(addPropertyV3(obj, '5555'));
+console.log(addPropertyV4(obj, '22666622'));
+
