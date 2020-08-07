f098cef3ad0e00e80d3a15326bc5f808d1118a93
Nazar <sikirinskijn@gmailcom>
2020-08-07 14:15:31 +0300

    vfr

---
+++ Lesson12/Task1/index.js
@@ -0,0 +1,15 @@
+const calc = expression => {
+  const [a, operator, b] = expression.split(' ');
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = Number(a) + +b;
+      break;
+    case '-':
+      result = a - b;
+      break;
+  }
+  return `${expression} = ${result}`;
+};
+console.log(calc('1007 + 2'));
