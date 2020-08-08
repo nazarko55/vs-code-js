c19b267ffcd2997372ed9449c9995fdda569a0cf
Nazar <sikirinskijn@gmailcom>
2020-08-08 17:13:21 +0300

    ttt

--- Lesson13/Task6/Lesson8/Task8/index.js
+++ Lesson13/Task6/Lesson8/Task8/index.js
@@ -1,6 +1,15 @@
-export function arrAverage(arr) {
-  if (!Array.isArray(arr)) return null;
-  return arr.reduce((acc, elem) => (acc + elem)) / arr.length;
+export const withdraw = (arr, clients, balances, client, amount) => {
+  if (Array.isArray(arr)) return null;
+  const similar = clients.indexOf(client);
+  console.log(clients.indexOf(client))
+  console.log(balances[similar])
+  const result = balances[similar] - amount;
+  // console.log(result)
+  if (result < 0) return -1;
+  return result;
 }
-const numbers = [25, 65, 19, 14, 21, 16, 17];
-console.log(arrAverage(numbers));
+
+const dataClientNames = ['Kate', 'Venom', 'Rex', 'Alan'];
+const dataClientBalances = [100, 50, 300, 5];
+
+console.log(withdraw(dataClientNames, dataClientBalances, 'Venom', 50));
--- Lesson13/Task6/Lesson8/Task8/index.test.js
+++ Lesson13/Task6/Lesson8/Task8/index.test.js
@@ -1,15 +1,25 @@
-import { arrAverage } from './index.js'
+import { getAdults } from './index.js';
 
-it('should return an array', () => {
-  const result = arrAverage([25, 65, 19, 14, 21, 16, 17]);
-  const checkType = typeof result === 'number';
-  expect(checkType).toEqual(true)
-})
-it('should return null if input is string', () => {
-  const result = arrAverage('string');
-  expect(result).toEqual(null)
-})
-it('should return an array', () => {
-  const result = arrAverage([10, 10, 10]);
-  expect(result).toEqual(10)
-})
+describe('arrayContaining', () => {
+  const expected = ['Kate', 'Venom'];
+  it('matches even if received contains additional elements', () => {
+    expect(['Kate', 'Venom', 'Eve']).toEqual(expect.arrayContaining(expected));
+  });
+  it('does not match if received does not contain expected elements', () => {
+    expect(['Venom', 'Eve']).not.toEqual(expect.arrayContaining(expected));
+  });
+});
+
+describe('Beware of a misunderstanding! A sequence of dice rolls', () => {
+  const expected = [100, 50, 300, 5];
+  it('matches even with an unexpected number 300', () => {
+    expect([50, 100, 300, 5, 50, 5]).toEqual(
+      expect.arrayContaining(expected),
+    );
+  });
+  it('does not match without an expected number 2', () => {
+    expect([100, 100, 300, 50, 5]).not.toEqual(
+      expect.arrayContaining(expected),
+    );
+  });
+});
