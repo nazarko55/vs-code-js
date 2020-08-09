7b34b99ee99c285174785a1176b088db9ebd7880
Nazar <sikirinskijn@gmailcom>
2020-08-09 22:51:39 +0300

    rr56r

---
+++ Lesson14/Task3/index.js
@@ -0,0 +1,20 @@
+let memory = 0;
+
+export function add(num) {
+  memory += num;
+};
+
+export function decrease(num) {
+  memory -= num;
+};
+
+export function reset(num) {
+  memory = 0;
+};
+
+export function getMemo(num) {
+  return memory;
+};
+
+//console.log(add(13));
+//console.log(decrease(41));
