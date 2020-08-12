16d658aba4bbdc0a9da7f2cbbfdfd54acb2e0064
Nazar <sikirinskijn@gmailcom>
2020-08-12 23:11:55 +0300

    hbjxhfv

---
+++ Lesson18/Task2/index.js
@@ -0,0 +1,11 @@
+const wallet = {
+  transaction: [1, 58, 9, 34, 956, 17, 83],
+  getMax() {
+    return Math.max(...this.transaction);
+  },
+  getMin() {
+    return Math.min(...this.transaction);
+  }
+}
+console.log(wallet.getMax());
+console.log(wallet.getMin());
