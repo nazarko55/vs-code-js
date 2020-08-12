183ccac22171862c38fe6a048496d10cf788ed26
Nazar <sikirinskijn@gmailcom>
2020-08-12 23:18:07 +0300

    jvgv

--- Lesson18/Task2/index.js
+++ Lesson18/Task2/index.js
@@ -1,11 +1,11 @@
 export const wallet = {
-  transaction: [1, 58, 9, 34, 956, 17, 83],
+  transactions: [1, 58, 9, 34, 956, 17, 83],
   getMax() {
-    return Math.max(...this.transaction);
+    return Math.max(...this.transactions);
   },
   getMin() {
-    return Math.min(...this.transaction);
-  }
+    return Math.min.apply(null, this.transactions);
+  },
 }
 console.log(wallet.getMax());
 console.log(wallet.getMin());
