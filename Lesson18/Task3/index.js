79c16f32cad7910b4c8d276c20fc73b52a2e7a83
Nazar <sikirinskijn@gmailcom>
2020-08-12 23:39:19 +0300

    ddd

--- Lesson18/Task3/index.js
+++ Lesson18/Task3/index.js
@@ -1,6 +1,6 @@
-export function argsSum() {
+export function sumOfSquares() {
   return [...arguments].reduce((acc, elem) => acc += elem * elem, 0);
 
 }
 
-console.log(argsSum(1, 4, 8, 5));
+console.log(sumOfSquares(1, 4, 8, 5));
