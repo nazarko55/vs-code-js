e1635e95194d59b030557ca8e4cff26a00b75c27
Nazar <sikirinskijn@gmailcom>
2020-08-12 22:55:56 +0300

    hmnb

---
+++ Lesson18/Task1/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const event = {
+  guests: [
+    { name: 'Tom', email: 't@gmail.com', age: 17 },
+    { name: 'John', email: 'example@server.com', age: 18 }
+  ],
+  message: 'Welcome to the party',
+  getInvitations() {
+    return this.guests
+      .filter(({ age }) => age >= 18)
+      .map(({ name, email }) => ({
+        text: `Hi ${name}, ${this.message}`,
+        email
+      }));
+  }
+}
+
+console.log(event.getInvitations());
