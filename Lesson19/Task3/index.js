a548d1844ca25ae5410c98def14eb52e91decedd
Nazar <sikirinskijn@gmailcom>
2020-08-13 16:34:44 +0300

    ddd

---
+++ Lesson19/Task3/index.js
@@ -0,0 +1,10 @@
+let obj = {};
+export const getOwnProps = obj => {
+  let propArr = [];
+  for (let prop in obj) {
+    if (typeof obj[prop] !== 'function' && obj.hasOwnProperty(prop)) {
+      propArr.push(prop);
+    }
+  }
+  return propArr;
+};
