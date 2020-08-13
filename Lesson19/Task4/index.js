3a8da09457c0a040acd2ed63cde5c4f58fdfd9bc
Nazar <sikirinskijn@gmailcom>
2020-08-13 19:09:22 +0300

    gfdgb

--- Lesson19/Task4/index.js
+++ Lesson19/Task4/index.js
@@ -4,15 +4,17 @@ export function User(name, age) {
 }
 
 User.prototype.sayHi = function () {
-  console.log(`Hi, I am ${this.name}`);
-}
+  console.log(`Hi, I am ${this.name}`)
+};
+
 User.prototype.requestNewPhoto = function () {
-  console.log(`New photo request was sent for ${this.name}`);
+  console.log(`New photo request was sent for ${this.name}`)
 }
-User.prototype.setAge = function () {
+
+User.prototype.setAge = function (age) {
   this.age = age;
-  if (age > 25) {
-    console.log(`New photo request was sent for ${this.name}`);
+  if (age >= 25) {
+    console.log(`New photo request was sent for ${this.name}`)
   }
   if (age < 0) {
     return false
