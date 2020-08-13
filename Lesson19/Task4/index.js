2dcf78f11e290a5f116f353f2569f91a76cdf00e
Nazar <sikirinskijn@gmailcom>
2020-08-13 19:06:50 +0300

    hg

---
+++ Lesson19/Task4/index.js
@@ -0,0 +1,22 @@
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+User.prototype.sayHi = function () {
+  console.log(`Hi, I am ${this.name}`);
+}
+User.prototype.requestNewPhoto = function () {
+  console.log(`New photo request was sent for ${this.name}`);
+}
+User.prototype.setAge = function () {
+  this.age = age;
+  if (age > 25) {
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+  if (age < 0) {
+    return false
+  } else if (age >= 0) {
+    return age;
+  }
+}
