ccc383a84903aa1b61b41e2a00b86532f00f5b7c
Nazar <sikirinskijn@gmailcom>
2020-08-13 23:42:46 +0300

    dd

--- Lesson20/Task4/index.js
+++ Lesson20/Task4/index.js
@@ -3,8 +3,8 @@ export class Order {
     this.id = (Math.random()).toString();
     this.price = price;
     this.dateCreated = new Date();
-    this.isConfirmed = 0;
-    this.dateConfirmed = false;
+    this.dateConfirmed = 0;
+    this.isConfirmed = false;
     this.city = city;
     this.type = type;
   }
@@ -12,18 +12,16 @@ export class Order {
   checkPrice() {
     return this.price > 1000;
   }
-
   confirmOrder() {
     if (!this.isConfirmed) {
       this.isConfirmed = true;
     }
     this.dateConfirmed = new Date();
-  }
 
+  }
   isValidType() {
     return (this.type === 'Buy' || this.type === 'Sell');
   }
 }
-
 const newBuy = new Order(100000, 'Odessa', 'Buy');
 console.log(newBuy);
