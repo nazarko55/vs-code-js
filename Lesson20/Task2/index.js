2429d1e85060b18c166108712e54acd2d5aae8f9
Nazar <sikirinskijn@gmailcom>
2020-08-13 20:51:47 +0300

    jhjgv

---
+++ Lesson20/Task2/index.js
@@ -0,0 +1,27 @@
+export class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.name = name;
+    this.numberOfWheels = numberOfWheels;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+export class Ship extends Vehicle {
+  constructor(name, maxSpeed) {
+    super(name, false);
+    this.maxSpeed = maxSpeed;
+  }
+  move() {
+    super.move();
+    console.log(`${this.name} lifting anchor up`);
+  }
+  stop() {
+    super.stop();
+    console.log(`${this.name} lifting anchor down`);
+  }
+}
