b389fcb2fc38bf8f7edff76273983bae3e445ca7
Nazar <sikirinskijn@gmailcom>
2020-08-13 23:37:54 +0300

    JNJ

---
+++ Lesson20/Task4/index.js
@@ -0,0 +1,29 @@
+EXPORT class Order {
+  constructor(price, city, type) {
+    this.id = (Math.random()).toString();
+    this.price = price;
+    this.dateCreated = new Date();
+    this.dateConfirmed = 0;
+    this.isConfirmed = false;
+    this.city = city;
+    this.type = type;
+  }
+
+  checkPrice() {
+    return this.price > 1000;
+  }
+
+  confirmOrder() {
+    if (!this.isConfirmed) {
+      this.isConfirmed = true;
+    }
+    this.dateConfirmed = new Date();
+  }
+
+  isValidType() {
+    return (this.type === 'Buy' || this.type === 'Sell');
+  }
+}
+
+const newBuy = new Order(100000, 'Odessa', 'Buy');
+console.log(newBuy);
