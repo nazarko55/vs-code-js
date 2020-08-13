666a418401d9728ca4c0ab9455869e45ce4d92ea
Nazar <sikirinskijn@gmailcom>
2020-08-13 15:39:19 +0300

    jvh

--- Lesson19/Task2/index.js
+++ Lesson19/Task2/index.js
@@ -4,15 +4,17 @@ const user = {
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   },
-  setFullName(value) {
+  set fullName(value) {
     const [firstName, lastName] = value.split(' ');
     this.firstName = firstName;
     this.lastName = lastName;
   },
 };
+
 export default user;
+
 console.log(user.fullName);
-user.setFullName = 'Tom Hardy';
+user.fullName = 'Tom Hardy';
 console.log(user.fullName);
 user.fullName = 'James Sallivan';
 console.log(user.firstName);
