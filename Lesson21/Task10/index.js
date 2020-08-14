c99e038629c319709f40df69eb4057126246b1e8
Nazar <sikirinskijn@gmailcom>
2020-08-14 22:53:35 +0300

    dd

---
+++ Lesson21/Task10/index.js
@@ -0,0 +1,10 @@
+const finishForm = () => {
+  const pasInput = document.querySelector('input[type = "text"]');
+  pasInput.type = 'password';
+  const loginInput = document.createElement('input');
+  loginInput.type = 'text';
+  loginInput.name = 'login';
+  const formElem = document.querySelector('.login=form');
+  formElem.prepend(loginInput);
+};
+export (finishForm);
