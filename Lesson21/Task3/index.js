8908bd5fe560fa879efee5112a76446d7949d166
Nazar <sikirinskijn@gmailcom>
2020-08-14 16:21:59 +0300

    yyy

---
+++ Lesson21/Task3/index.js
@@ -0,0 +1,14 @@
+function getItemsList() {
+  let elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+}
+function getItemsArray() {
+  let elementsArray = Array.from(document.querySelectorAll('.tool'));
+  console.dir(elementsArray);
+  return elementsArray;
+
+}
+getItemsArray();
+getItemsList();
+export { getItemsList, getItemsArray }
