f224bfe87b7ebe2908aae29a1a734900b6c87098
Nazar <sikirinskijn@gmailcom>
2020-08-22 00:04:07 +0300

    dd

--- Lesson24/Task3/index.js
+++ Lesson24/Task3/index.js
@@ -1,4 +1,4 @@
-function getDiff(startDate, endDate) {
+export function getDiff(startDate, endDate) {
   const millSecPerMinute = 60 * 1000;
   const millSecPerHour = 60 * millSecPerMinute;
   const millSecPerDay = 24 * millSecPerHour;
@@ -14,8 +14,3 @@ function getDiff(startDate, endDate) {
   return `${countDays}d ${countHours}h ${countMinutes}m ${countSeconds}s`;
 }
 
-export { getDiff };
-
-startDate = new Date(2019, 11, 3, 12, 20, 30);
-endDate = new Date(2019, 11, 7, 11, 40, 23);
-console.log(getDiff(startDate, endDate));
