92f3d83270761890fba0f5379106beb31601ca25
Nazar <sikirinskijn@gmailcom>
2020-08-21 20:03:00 +0300

    d

--- Lesson24/Task1/index.js
+++ Lesson24/Task1/index.js
@@ -1,8 +1,11 @@
-const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']; \
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
-export const DayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
-
   return weekDays[new Date(dateInFuture).getDay()];
-};
+}
+    // const result = dayOfWeek(new Date(2019, 0, 1), 12);
+    // console.log(result);
+
+// console.log(weekDays[result]);
