317f1bdaaac3b06ea303e944d5f16c51ca31a9fc
Nazar <sikirinskijn@gmailcom>
2020-08-24 10:22:00 +0300

    bjh

---
+++ Lesson26/Task1/index.js
@@ -0,0 +1,32 @@
+
+const calc = initialVal => {
+  let result = initialVal;
+
+  const calculator = {
+    add(value) {
+      result += value;
+      return this;
+    },
+    mult(value) {
+      result *= value;
+      return this;
+    },
+    substract(value) {
+      result -= value;
+      return this;
+    },
+    div(value) {
+      result /= value;
+      return this;
+    },
+    result() {
+      return result;
+    }
+  };
+  return calculator;
+};
+
+
+const result = calc(10).add(2).mult(5).div(3).substract(5).result();
+
+console.log(result);
