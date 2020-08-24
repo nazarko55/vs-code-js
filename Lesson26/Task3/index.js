1eb802bd08651d9283e7249c749daf89a986f769
Nazar <sikirinskijn@gmailcom>
2020-08-24 13:24:15 +0300

    ,njk

--- Lesson26/Task3/index.js
+++ Lesson26/Task3/index.js
@@ -2,8 +2,8 @@ const mult = a => b => a * b;
 
 const twice = mult(2);
 
-const triple = mult(5);
+const triple = mult(3);
 
-console.log(triple(3));
+console.log(triple(5));
 
-export { mult, twice, triple };
+//export { mult, twice, triple };
