2da6b4c9d2b6c487aebb8c32037f8b5c755c9bfb
Nazar <sikirinskijn@gmailcom>
2020-08-25 16:17:08 +0300

    cmc

--- Lesson27/Task1/index.js
+++ Lesson27/Task1/index.js
@@ -14,7 +14,7 @@ const addImage = (imgSrc, callback) => {
   imgElem.addEventListener('error', () => callback('Image load is failed...'));
 
 };
-
+const imgSrc = ("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg");
 const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
