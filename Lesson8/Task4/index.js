7b809addfe35a14df6de0416d0bba4e3f3223d7b
Nazar <sikirinskijn@gmailcom>
2020-08-25 12:53:26 +0300

    kjn

--- Lesson8/Task4/index.js
+++ Lesson8/Task4/index.js
@@ -1,12 +1,64 @@
-const user = {
-  name: 'Tom',
-  age: 17,
-};
-user.hobby = 'football';
-const anotherKey = 'married';
-user.married = false;
-user['favorite music'] = 'rock';
-user.address = {};
-user.address.country = 'Ukraine';
-user.address.building = 17;
-console.log(user);
+//'use strict';
+function printMessage(age, city) {
+  console.log(this);
+  console.log(`Hello ${this.name}, email ${this.email}, age: ${age}, city: ${city}`);
+}
+let user = {
+  name: "Alex",
+  email: "gmail.com",
+}
+
+//1
+
+printMessage(100, 'Lviv');
+
+//const printMessageDinded = printMessage.bind(user);
+
+//printMessageDinded(100, 'Lviv');
+
+//printMessage.bind(user, 100, 'Lviv');
+
+
+const myBind = (func, context, ...ragss) => {
+  console.log('step 1');
+  return function () {
+    console.log('step 2');
+    return func.call(context, ...ragss);
+  }
+}
+
+const printMessageDinded = myBind(printMessage, user, 200, 'Lviv');
+printMessageDinded();
+
+
+const myBind1 = (func, context, ...ragss) => {
+  console.log('step 1');
+  return function (...targetArgs) {
+    console.log('step 2');
+    return func.call(context, ...ragss, ...targetArgs);
+  }
+}
+
+const printMessageDinded1 = myBind1(printMessage, user);
+printMessageDinded1(200, 'Kiev');
+
+
+const printMessageDinded2 = myBind2(printMessage, user, 200);
+printMessageDinded2('Kharkiv');
+
+
+
+const mySuperBind1 = (func, context, ...ragss) => {
+  return function () {
+
+    console.log('context');
+    context.tempFunction = func;
+
+    console.log(context);
+    context.tempFunction(...ragss);
+  }
+}
+}
+
+const printMessageDinded3 = mySuperBind1(printMessage, user, 50, 'dnipro');
+printMessageDinded3();
