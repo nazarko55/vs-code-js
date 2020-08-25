900bf3e35aebf30f814252e539a4fba35388b7ae
Nazar <sikirinskijn@gmailcom>
2020-08-25 17:53:50 +0300

    kv

--- Lesson27/Task1/index.js
+++ Lesson27/Task1/index.js
@@ -1,18 +1,23 @@
-export function addImage(url, callback) {
-  const pageElem = document.querySelector('.page');
-
+const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'Some picture');
-  imgElem.src = url
-
-  pageElem.append(imgElem)
-  imgElem.addEventListener('load', () => {
+  imgElem.setAttribute('alt', 'My photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+  const onImageLoaded = () => {
     const { width, height } = imgElem;
-    callback(null, { width, height })
-  })
-
-  imgElem.addEventListener('error', () => {
-    const errorMessage = 'Image load failed'
-    callback(errorMessage)
-  })
-}
+    callback(null, { width, height });
+  }
+  imgElem.addEventListener('load', onImageLoaded);
+  imgElem.addEventListener('error', () => callback('Image load failed'));
+};
+const onImageLoaded = (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = data;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+export { addImage };
