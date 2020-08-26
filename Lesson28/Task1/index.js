75372dbb023fb32d4abc07ca299188c2e20e0256
Nazar <sikirinskijn@gmailcom>
2020-08-26 12:59:31 +0300

    jvgh

--- Lesson28/Task1/index.js
+++ Lesson28/Task1/index.js
@@ -1,22 +1,21 @@
-export const addImage = imgSrc => {
-  return new Promise((getImageSize, reject) => {
+const addImage = imgSrc => {
+  const p = new Promise((resolve, reject) => {
     const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.setAttribute('alt', 'My photo');
     imgElem.src = imgSrc;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
 
-    function onImageLoaded() {
+    const onImgLoaded = () => {
       const { width, height } = imgElem;
-      getImageSize({ width, height });
-    };
+      resolve({ width, height });
+    }
 
-    imgElem.addEventListener('load', onImageLoaded);
-    imgElem.addEventListener('error', () => reject(new Error('Image load failed')));
+    imgElem.addEventListener('load', onImgLoaded);
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed...')));
   });
+  return p;
 };
 
-const imgSrc = 'htps://wallpapershome.com/images/wallpapers/rihan-1080x1920-top-music-artist-and-bands-singer-actress-510.jpg';
+const imgSrc = 'https://wallpapershome.com/images/wallpapers/rihan-1080x1920-top-music-artist-and-bands-singer-actress-510.jpg';
 
 const resultPromise = addImage(imgSrc);
 
