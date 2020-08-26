0c1a0d3acc0ecd24bf6e08a832c7eb54f04dea32
Nazar <sikirinskijn@gmailcom>
2020-08-26 16:25:12 +0300

    ,HB

---
+++ Lesson28/Task3/index.js
@@ -0,0 +1,32 @@
+/* ===> 1 <=== */
+
+/*
+ * successPromise должен зарезолвить число 67
+ * Ответьте себе на вопрос, какой тип данных имеет переменная successPromise
+ */
+
+const successPromise = new Promise(resolve => {
+  /* ...code here */
+  return resolve(67);
+}, reject => reject(new Error('I am an error')));
+console.log(successPromise);
+
+/*
+* допишите обработчик успешного промиса (аргументы и тело ф-ции onSuccess)
+* чтобы в консоль вывелся квадрат числа, которое резолвиться из successPromise
+*/
+
+successPromise.then(function onSuccess(number) {
+  /* ...code here */
+  console.log(number ** 2);
+});
+
+/*
+* в обработчике ошибок промиса (ф-ция onError внутри .catch()) выведите в консоль текст 'I am an error'
+* текст вывелся в консоль?
+* подумайте почему
+*/
+successPromise.catch(function onError(error) {
+  /* ...code here */
+  console.log(error);
+});
