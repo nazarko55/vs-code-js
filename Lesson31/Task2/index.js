1a43e0143be38b805f3fa131e5b401c85fe70b1a
Nazar <sikirinskijn@gmailcom>
2020-08-31 19:49:43 +0300

    jg

---
+++ Lesson31/Task2/index.js
@@ -0,0 +1,14 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+const getTasksList = () => {
+  return fetch(baseUrl)
+    .then(response => response.json())
+    .then(result => console.log(result))
+
+};
+
+function getTaskById(tasksId) {
+  return fetch(`https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks/${tasksId}`)
+    .then(response => response.json())
+    .then(result => console.log(result));
+}
