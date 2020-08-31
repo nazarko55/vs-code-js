04ed5924630f39ce34edc0a5af0b18e28d8cd4d6
Nazar <sikirinskijn@gmailcom>
2020-08-31 19:04:58 +0300

    jhb

---
+++ Lesson31/Task1/index.js
@@ -0,0 +1,42 @@
+const userNameElem = document.querySelector('.user__name');
+const userLocationElem = document.querySelector('.user__location');
+const userAvatarElem = document.querySelector('.user__avatar');
+
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001'
+
+userAvatarElem.src = defaultAvatar;
+
+//https://api.github.com/users/USERNAME
+
+// fetch and render user data...
+
+
+
+
+const fetchUserData = userName => {
+  return fetch(`https://api.github.com/users/${userName}`)
+    .then(response => response.json());
+}
+
+const renderUserData = userData => {
+  const { avatar_url, name, location } = userData;
+  userAvatarElem.src = avatar_url;
+  userNameElem.textContent = name;
+  userLocationElem.textContent = location
+    ? `from ${location}`
+    : '';
+
+}
+fetchUserData()
+
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+const onSearchUser = () => {
+  const userName = userNameInputElem.value;
+  fetchUserData(userName)
+    .then(userData => renderUserData(userData));
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser)
