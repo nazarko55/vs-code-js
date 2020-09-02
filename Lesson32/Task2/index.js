b69559cda6f4579aefcd32c77eeb1b11195baf43
Nazar <sikirinskijn@gmailcom>
2020-09-02 13:20:30 +0300

    kdsjf

---
+++ Lesson32/Task2/index.js
@@ -0,0 +1,50 @@
+const baseUrl = 'https://5f4f63495b92f4001604e527.mockapi.io/api/v1/users';
+
+const getUsersList = () => {
+  return fetch(baseUrl)
+    .then(response => response.json())
+};
+
+const getUserById = (userId) => {
+  return fetch(`${baseUrl}/ ${userId}`)
+    .then(response => response.json())
+}
+
+const createUser = (userObj) => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userObj)
+  })
+};
+
+//const newUserData = {
+//email: "cool@email.com",
+//firstName: 'Iron',
+//lastName: 'Man',
+// age: 42,
+//};
+//createUser(newUserData)
+//  .then(() => console.log('User created'));
+
+
+function updateUser(userId, userObj) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userObj)
+  })
+};
+
+
+const deleteUser = (userId) => {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'DELETE',
+  })
+};
+
+export { deleteUser, updateUser, createUser, getUserById, getUsersList };
